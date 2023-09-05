169355bf682f8bad1085bd8899d9cecd5930d00b
jbresser <98827419+jbresser@users.noreply.github.com>
2023-09-05 17:00:26 +0200

    haiyaaa

---
+++ scripts/tester/tester9.js
@@ -0,0 +1,20 @@
+console.log('script loading')
+const dropList = document.getElementById("selectOptionDataset")
+
+dropList.addEventListener('click', function () {
+    const popupContent = document.getElementById('popupContent');
+    const popup = document.getElementById('popup');
+    const overlay = document.getElementById('overlay');
+    
+    overlay.style.display = 'block';
+    popup.style.display = 'block';
+    popupContent.innerHTML = '<div class="loader" ></div>' 
+
+    console.log('trigger worked')
+})
+
+prettyPlease = function() {
+    console.log("I am here")
+}
+
+console.log('script loaded')
