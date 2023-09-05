758e4202033941f8ab41ac64af980464cf03414c
jbresser <98827419+jbresser@users.noreply.github.com>
2023-09-05 13:40:51 +0200

    Update tester.js

--- scripts/tester/tester.js
+++ scripts/tester/tester.js
@@ -1,5 +1,24 @@
 
 console.log('script loaded')
+const dropList = document.getElementById("selectOptionDataset")
+
+dropList.addEventListener('click', function () {
+    const popupContent = document.getElementById('popupContent');
+    const popup = document.getElementById('popup');
+    const overlay = document.getElementById('overlay');
+    
+    const closeButton = document.createElement('button');
+    closeButton.id = 'closeButton';
+    closeButton.className = 'close-button';
+    closeButton.textContent = 'x';
+    closeButton.addEventListener('click', closePopup);
+    
+    overlay.style.display = 'block';
+    popup.style.display = 'block';
+    popupContent.innerHTML = '<div class="loader" ></div>' 
+
+    console.log('trigger worked')
+})
 
 prettyPlease = function() {
     console.log("I am here")
